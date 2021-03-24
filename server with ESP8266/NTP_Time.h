267bce062e1430aabe4085c955ef70a099f09a17
Norbert Gal <gnorbert@protonmail.com>
2021-03-24 20:50:33 +0100

    Repo split to client code and ubuntu server part. From that point this repo contains source code for watering system client hardware.

--- server with ESP8266/NTP_Time.h
+++
@@ -1,7 +0,0 @@
-#ifndef NTP_Time_H_
-#define NTP_Time_H_
-
-time_t getTime();
-
-
-#endif
--- server_on_ubuntu/server/MySQL/mqtt_mysql_handler.sh
+++
@@ -1,4 +0,0 @@
-sleep 10
-#PYTHONPATH=/home/odroid/.local/lib/python2.7/site-packages
-filename="python-$(date +%Y-%V).log"
-python2.7 -u /home/odroid/Desktop/server/MySQL/mqtt_mysql_handler.py 2>&1 | while IFS= read -r line; do echo "$(date +%x__%H:%M:%S:%N) $line"; done >> "/home/odroid/logs/$filename" #u unbuffered  output, for logging, add time stuff
